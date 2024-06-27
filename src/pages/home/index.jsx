ab376e3ff2b897af7ea66f735b687ae8fee9ee87
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2024-06-26 21:36:36 -0300

    feat: implementa melhorias na página de home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import styles from './home.module.scss';
+import Input from 'components/input';
+import { ReactComponent as MenssageIcon } from '../../assets/menssageIcon.svg';
 
 export default function Home() {
   return (
-    <div className={styles.container}>testando rotas</div>
+    <div className={styles.container}>
+      <div className={styles.container__template}>
+        <aside className={styles.container__template__aside}>
+
+        </aside>
+
+        <div className={styles.container__template__main}>
+          <header>
+            <Input type={'text'} placeholder={'Serach'} />
+
+            <span>
+              <MenssageIcon />
+              <div className={styles.container__template__main__image}></div>
+            </span>
+
+          </header>
+
+        </div>
+      </div>
+    </div>
   );
 }
