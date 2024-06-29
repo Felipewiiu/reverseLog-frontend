aebb4918fe126be8c0db9a2485c1335f06275c34
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2024-06-29 08:34:02 -0300

    feat: adiciona link de rotas ao dashboard

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,7 +7,7 @@ import { ReactComponent as HomeIcon } from '../../assets/home.svg';
 import { ReactComponent as ContractIcon } from '../../assets/contract.svg';
 import { ReactComponent as Suport } from '../../assets/suport.svg';
 import { ReactComponent as RequestIcon } from '../../assets/description.svg';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 
 export default function Home() {
@@ -22,16 +22,43 @@ export default function Home() {
         <aside className={styles.container__template__aside}>
           <Smartlog />
           <ul className={styles.container__template__aside__box}>
-            <li><span><HomeIcon /></span>Dashboard</li>
-            <li><span><ContractIcon /></span>Cadastro</li>
-            <li><span><RequestIcon /></span>Solicitação de RMA</li>
-            <li><span><Suport /></span>Fale conosco</li>
+            <Link to={'/home'} className={styles.Link}>
+              <li>
+                <span>
+                  <HomeIcon />
+                </span>
+
+                Dashboard
+              </li>
+            </Link>
+
+            <Link to={'/home/cadastro'} className={styles.Link}>
+              <li>
+                <span>
+                  <ContractIcon />
+                </span>
+                Cadastro
+              </li>
+            </Link>
+
+            <li>
+              <span>
+                <RequestIcon />
+              </span>
+              Solicitação de RMA
+            </li>
+            <li>
+              <span>
+                <Suport />
+              </span>
+              Fale conosco
+            </li>
           </ul>
         </aside>
 
         <div className={styles.container__template__main}>
           <header>
-            <Input type={'text'} placeholder={'Serach'} change={setSearch} value={search} valueWidh={'500px'} borderRadiusValue={'12px'}/>
+            <Input type={'text'} placeholder={'Serach'} change={setSearch} value={search} valueWidh={'500px'} borderRadiusValue={'12px'} />
 
             <span>
               <MenssageIcon style={{ cursor: 'pointer' }} />
@@ -42,11 +69,11 @@ export default function Home() {
           </header>
 
           <div className={styles.container__template__main__boxContainer}>
-            <Outlet/>
+            <Outlet />
           </div>
 
         </div>
       </div>
-    </div>
+    </div >
   );
 }
