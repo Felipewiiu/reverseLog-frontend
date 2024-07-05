7933c21c22c582818026f2de0ffa5a2ff5e34d90
felipewiiu3 <felipewiiu3@gmail.com>
2024-07-05 10:52:21 -0300

    fix: corrige o objeto do payload da rota de cadastro

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -14,10 +14,7 @@ export default function Register() {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
   const [name, setName] = useState();
-  const [email, setEmail] = useState();
   const [phone, setPhone] = useState();
-  const [password, setPassword] = useState();
-  const [cpf, setCpf] = useState();
   const [cnpj, setCnpj] = useState();
   const [publicPlace, setPublicPlace] = useState();
   const [neighborhood, setNeighborhood] = useState();
@@ -29,27 +26,28 @@ export default function Register() {
 
   const emailVerify = parseJwt(sessionStorage.getItem('token'));
   console.log(emailVerify.sub);
+  console.log(sessionStorage.getItem('token'));
 
   const submit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const payload = {
-      'name': name,
-      'email': email,
       'telefone': phone,
-      'cpf': cpf,
       'cnpj': cnpj,
-      'logradouro': publicPlace,
-      'bairro': neighborhood,
-      'cep': cep,
-      'numero': number,
-      'complemento': complement,
-      'cidade': city,
-      'uf': uf,
-      'password': password
+      'endereco': {
+        'logradouro': publicPlace,
+        'bairro': neighborhood,
+        'cep': cep,
+        'numero': number,
+        'complemento': complement,
+        'cidade': city,
+        'uf': uf,
+      },
+      'name': name
+
     };
 
-    http.put(`clientes/update-custumer?email=${emailVerify.sub}`, payload )
+    http.put(`clientes/update-custumer?email=${emailVerify.sub}`, payload)
       .then((response) => {
         console.log(response);
       })
@@ -86,21 +84,19 @@ export default function Register() {
           <form className={styles.container__template__box__form} id='formUpdate'>
             <div >
               <Input label={'Nome'} marginBotton={'1rem'} change={setName} placeholder={'Digite o seu nome'} />
-              <Input label={'E-mail'} marginBotton={'1rem'} change={setEmail} placeholder={'Digite o seu E-mail'} />
               <Input label={'Telefone'} marginBotton={'1rem'} change={setPhone} placeholder={'Digite o seu telefone'} />
-              <Input label={'Cpf'} marginBotton={'1rem'} change={setCpf} placeholder={'Digite o seu CPF'} />
               <Input label={'CNPJ'} marginBotton={'1rem'} change={setCnpj} placeholder={'Digite o seu CNPJ'} />
               <Input label={'UF'} marginBotton={'1rem'} change={setUf} placeholder={'Digite o seu distrito'} />
+              <Input label={'Logradouro'} marginBotton={'1rem'} change={setPublicPlace} placeholder={'Digite o seu logradouro'} />
 
             </div>
             <div  >
-              <Input label={'Logradouro'} marginBotton={'1rem'} change={setPublicPlace} placeholder={'Digite o seu logradouro'} />
               <Input label={'Bairro'} marginBotton={'1rem'} change={setNeighborhood} placeholder={'Digite o seu bairro'} />
               <Input label={'CEP'} marginBotton={'1rem'} change={setCep} placeholder={'Digite o seu CEP'} />
               <Input label={'Número'} marginBotton={'1rem'} change={setNumber} placeholder={'Digite o seu número'} />
               <Input label={'Complemento'} marginBotton={'1rem'} change={setComplement} placeholder={'Digite o seu complemento'} />
               <Input label={'Cidade'} marginBotton={'1rem'} change={setCity} placeholder={'Digite a sua cidade'} />
-              
+
             </div>
 
           </form>
