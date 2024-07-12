296f40eb35d7c51a83415c178b3babd51ae38859
felipewiiu3 <felipewiiu3@gmail.com>
2024-07-12 14:58:25 -0300

    bugfix: Corrige os campos de atualizações que estavam trocados

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './register.module.scss';
 import Input from 'components/input';
 import Button from 'components/button';
@@ -9,24 +9,25 @@ import useHttp from 'HTTP';
 import parseJwt from '../../security/securityValidationId';
 
 
+
 export default function Register() {
+
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [publicPlace, setPublicPlace] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [cep, setCep] = useState('');
+  const [number, setNumber] = useState('');
+  const [complement, setComplement] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
 
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-  const [name, setName] = useState();
-  const [phone, setPhone] = useState();
-  const [cnpj, setCnpj] = useState();
-  const [publicPlace, setPublicPlace] = useState();
-  const [neighborhood, setNeighborhood] = useState();
-  const [cep, setCep] = useState();
-  const [number, setNumber] = useState();
-  const [complement, setComplement] = useState();
-  const [city, setCity] = useState();
-  const [uf, setUf] = useState();
+  const emailVerify = parseJwt(sessionStorage.getItem('token'));
   const http = useHttp();
 
-  const emailVerify = parseJwt(sessionStorage.getItem('token'));
-  
   const submit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -49,15 +50,51 @@ export default function Register() {
     http.put(`clientes/update-custumer?email=${emailVerify.sub}`, payload)
       .then((response) => {
         console.log('Cadastro atualizado');
+        reloadData();
       })
       .catch(error => {
         console.log(sessionStorage.getItem('token'));
-        
-      });
 
+      });
 
   };
 
+  const reloadData = () =>{
+    http.get(`clientes/get-email?email=${emailVerify.sub}`)
+      .then(response => {
+
+        const data = response.data;
+        
+        setName(data.name);
+
+        setPhone(data.telefone);
+
+        setCnpj(data.cnpj);
+
+        setPublicPlace(data.logradouro);
+
+        setNeighborhood(data.bairro);
+
+        setCep(data.cep);
+
+        setNumber(data.numero);
+
+        setComplement(data.complemento);
+
+        setCity(data.cidade);
+
+        setUf(data.uf);
+
+
+      })
+      .catch(error => {
+        console.log('Erro ao recarregar dados ' + error);
+      });
+  };
+
+  useEffect(() => {
+    reloadData();
+  }, []);
 
 
   const closeTable = () => {
@@ -83,19 +120,19 @@ export default function Register() {
 
           <form className={styles.container__template__box__form} id='formUpdate'>
             <div>
-              <Input label={'Nome'} marginBotton={'1rem'} change={setName} placeholder={'Digite o seu nome'} bottom={'10px'} />
-              <Input label={'Telefone'} marginBotton={'1rem'} change={setPhone} placeholder={'Digite o seu telefone'} bottom={'10px'} />
-              <Input label={'CNPJ'} marginBotton={'1rem'} change={setCnpj} placeholder={'Digite o seu CNPJ'} bottom={'10px'} />
-              <Input label={'UF'} marginBotton={'1rem'} change={setUf} placeholder={'Digite o seu distrito'} bottom={'10px'} />
-              <Input label={'Logradouro'} marginBotton={'1rem'} change={setPublicPlace} placeholder={'Digite o seu logradouro'} bottom={'10px'} />
+              <Input label={'Nome'} marginBotton={'1rem'} change={setName} placeholder={'Digite o seu nome'} bottom={'10px'} value={name} />
+              <Input label={'Telefone'} marginBotton={'1rem'} change={setPhone} placeholder={'Digite o seu telefone'} bottom={'10px'} value={phone} />
+              <Input label={'CNPJ'} marginBotton={'1rem'} change={setCnpj} placeholder={'Digite o seu CNPJ'} bottom={'10px'} value={cnpj} />
+              <Input label={'UF'} marginBotton={'1rem'} change={setUf} placeholder={'Digite o seu distrito'} bottom={'10px'} value={uf} />
+              <Input label={'Logradouro'} marginBotton={'1rem'} change={setPublicPlace} placeholder={'Digite o seu logradouro'} bottom={'10px'} value={publicPlace} />
 
             </div>
             <div  >
-              <Input label={'Bairro'} marginBotton={'1rem'} change={setNeighborhood} placeholder={'Digite o seu bairro'} bottom={'10px'} />
-              <Input label={'CEP'} marginBotton={'1rem'} change={setCep} placeholder={'Digite o seu CEP'} bottom={'10px'} />
-              <Input label={'Número'} marginBotton={'1rem'} change={setNumber} placeholder={'Digite o seu número'} bottom={'10px'} />
-              <Input label={'Complemento'} marginBotton={'1rem'} change={setComplement} placeholder={'Digite o seu complemento'} bottom={'10px'} />
-              <Input label={'Cidade'} marginBotton={'1rem'} change={setCity} placeholder={'Digite a sua cidade'} bottom={'10px'} />
+              <Input label={'Bairro'} marginBotton={'1rem'} change={setNeighborhood} placeholder={'Digite o seu bairro'} bottom={'10px'} value={neighborhood} />
+              <Input label={'CEP'} marginBotton={'1rem'} change={setCep} placeholder={'Digite o seu CEP'} bottom={'10px'} value={cep} />
+              <Input label={'Número'} marginBotton={'1rem'} change={setNumber} placeholder={'Digite o seu número'} bottom={'10px'} value={number} />
+              <Input label={'Complemento'} marginBotton={'1rem'} change={setComplement} placeholder={'Digite o seu complemento'} bottom={'10px'} value={complement} />
+              <Input label={'Cidade'} marginBotton={'1rem'} change={setCity} placeholder={'Digite a sua cidade'} bottom={'10px'} value={city} />
 
             </div>
 
