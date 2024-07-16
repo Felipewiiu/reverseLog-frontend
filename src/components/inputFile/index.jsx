dbe7e74c7a94ecf40ad7a2a63aefdb45765162d3
felipewiiu3 <felipewiiu3@gmail.com>
2024-07-16 13:04:37 -0300

    styles: refatora o botão de input

--- src/components/inputFile/index.jsx
+++ src/components/inputFile/index.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './inputFile.module.scss';
 
 export default function InputFile() {
+
+  const [fileName, setFileName] = useState('');
+
+  const handleFileChange = (e) => {
+    const  file = e.target.files[0];
+    console.log(file);
+
+    if(file) {
+      setFileName(file.name);
+    }else{
+      setFileName('');
+    }
+  };
+
   return (
-    <input type="file" />
+    <div className={styles.container}>
+      <input
+        type='file'
+        className={styles.file_input}
+        id='upload'
+        onChange={handleFileChange}
+      />
+
+      <label
+        htmlFor='upload'
+        className={styles.file_label}>
+        Escolher Arquivo
+      </label>
+
+      <div
+        className={styles.file_name}
+        id='file-name'
+      >
+        {fileName}
+      </div>
+    </div>
   );
 }
